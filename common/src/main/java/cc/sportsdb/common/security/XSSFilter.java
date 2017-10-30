4395db7a4e71334426dc20d16093892a687315fe
liangwenwei <liangwenwei@tuandai.com>
2017-10-30 18:24:10 +0800

    ...

---
+++ common/src/main/java/cc/sportsdb/common/security/XSSFilter.java
@@ -0,0 +1,30 @@
+package cc.sportsdb.common.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "xssFilter", urlPatterns = "/*", asyncSupported = true)
+public class XSSFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(XSSFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("XSSFilter initialize.");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(new XSSHttpServletRequest((HttpServletRequest) servletRequest), servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ common/src/main/java/cc/sportsdb/common/security/XSSHttpServletRequest.java
@@ -0,0 +1,71 @@
+package cc.sportsdb.common.security;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.util.Map;
+
+public class XSSHttpServletRequest extends HttpServletRequestWrapper {
+
+    private static final Whitelist whitelist = Whitelist.basicWithImages();
+
+    XSSHttpServletRequest(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getParameter(String name) {
+        return clean(super.getParameter(name));
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        Map<String, String[]> map = super.getParameterMap();
+        if (map == null || map.isEmpty()) {
+            return map;
+        }
+
+        for (Map.Entry<String, String[]> entry : map.entrySet()) {
+            String[] values = entry.getValue();
+            if (values == null) {
+                continue;
+            }
+
+            String[] newValues = new String[values.length];
+            for (int i = 0; i < values.length; i++) {
+                newValues[i] = clean(values[i]);
+            }
+
+            map.put(entry.getKey(), newValues);
+        }
+
+        return map;
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] values = super.getParameterValues(name);
+
+        if (values == null) {
+            return values;
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != null) {
+                values[i] = clean(values[i]);
+            }
+        }
+
+        return values;
+    }
+
+    private static String clean(String value) {
+        if (value == null) {
+            return null;
+        }
+        return Jsoup.clean(value, whitelist);
+    }
+
+}
