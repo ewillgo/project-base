1457b21c961777ca262bbca08e79fded7a064277
liangwenwei <liangwenwei@tuandai.com>
2017-12-25 18:32:02 +0800

    ...

--- common/src/main/java/cc/sportsdb/common/config/WebConfig.java
+++ common/src/main/java/cc/sportsdb/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package cc.sportsdb.common.config;
 import cc.sportsdb.common.data.mq.MqConfig;
 import cc.sportsdb.common.data.redis.RedisConfig;
 import cc.sportsdb.common.database.DataSourceConfig;
+import cc.sportsdb.common.database.MyBatisConfig;
 import cc.sportsdb.common.http.RestTemplateConfig;
 import cc.sportsdb.common.log.LoggingProperties;
 import cc.sportsdb.common.log.SpringMvcLoggingFilter;
@@ -30,6 +31,7 @@ import java.util.Arrays;
 @ImportAutoConfiguration({
         WebMvcConfig.class,
         DataSourceConfig.class,
+        MyBatisConfig.class,
         RestTemplateConfig.class,
         RedisConfig.class,
         MqConfig.class
